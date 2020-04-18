6d85c0ab8049f8d5a5f653c19d471a429191387a
bryht <bryht@live.com>
2020-04-18 21:48:31 +0200

    add id

--- pipeline/entrypoint.sh
+++ pipeline/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# if [ -z "$REACT_APP_APP_ID" ]
-# then
-#     echo "\$REACT_APP_APP_ID is empty"
-# else
-#     find ./ -type f -exec sed -i -e "s/{REACT_APP_APP_ID_PLACEHOLDER}/$REACT_APP_APP_ID/g" {} \;
-# fi
+if [ -z "$REACT_APP_APP_ID" ]
+then
+    echo "\$REACT_APP_APP_ID is empty"
+else
+    find ./ -type f -exec sed -i -e "s/{REACT_APP_APP_ID_PLACEHOLDER}/$REACT_APP_APP_ID/g" {} \;
+fi
 
 # if [ -z "$REACT_APP_SECRET" ]
 # then
