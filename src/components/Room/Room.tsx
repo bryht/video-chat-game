5be21e248a4d2512cb9fed76c088993027db43a0
bryht <bryht@live.com>
2020-04-19 17:42:41 +0200

    add switch

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -8,6 +8,8 @@ export interface IRoomProps {
 export interface IRoomStates {
   centerStream: VideoStream | null;
   rightStreams: Array<VideoStream>;
+  isVideoOn: boolean;
+  isAudioOn: boolean;
 
 }
 export default class Room extends React.Component<IRoomProps, IRoomStates> {
@@ -15,14 +17,18 @@ export default class Room extends React.Component<IRoomProps, IRoomStates> {
   appId: string;
   client: VideoClient;
   streamList: Array<VideoStream>;
+  localStream: VideoStream | null;
   constructor(props: Readonly<IRoomProps>) {
     super(props);
     this.appId = process.env.REACT_APP_APP_ID ?? '';
     this.client = new VideoClient(this.appId);
     this.streamList = [];
+    this.localStream = null;
     this.state = {
       centerStream: null,
-      rightStreams: []
+      rightStreams: [],
+      isAudioOn: true,
+      isVideoOn: true
     }
   }
 
@@ -30,6 +36,7 @@ export default class Room extends React.Component<IRoomProps, IRoomStates> {
   async componentDidMount() {
     this.client.onStreamListChanged = list => {
       this.streamList = list;
+      this.localStream = list.find(p => p.isLocal) ?? null;
       this.refreshVideos();
     };
     await this.client.create('123', Guid.newGuid());
@@ -37,9 +44,9 @@ export default class Room extends React.Component<IRoomProps, IRoomStates> {
 
   switchToCenter = (streamId: string) => {
     var maxOrder = Math.max(...this.streamList.map(p => p.order));
-    var stream=this.streamList.find(p=>p.id===streamId);
+    var stream = this.streamList.find(p => p.id === streamId);
     if (stream) {
-      stream.order=maxOrder+1;
+      stream.order = maxOrder + 1;
       this.refreshVideos();
     }
   }
@@ -57,6 +64,34 @@ export default class Room extends React.Component<IRoomProps, IRoomStates> {
     this.client.dispose();
   }
 
+  switchVideo = () => {
+    if (this.localStream) {
+      const { isVideoOn } = this.state;
+
+      if (isVideoOn) {
+        this.localStream.stream.muteVideo();
+        this.setState({ isVideoOn: false });
+      } else {
+        this.localStream.stream.unmuteVideo();
+        this.setState({ isVideoOn: true });
+      }
+    }
+  }
+
+  switchAudio = () => {
+    if (this.localStream) {
+      const { isAudioOn } = this.state;
+
+      if (isAudioOn) {
+        this.localStream.stream.muteAudio();
+        this.setState({ isAudioOn: false });
+      } else {
+        this.localStream.stream.unmuteAudio();
+        this.setState({ isAudioOn: true });
+      }
+    }
+  }
+
   public render() {
     const { centerStream, rightStreams } = this.state;
     return (
@@ -66,14 +101,11 @@ export default class Room extends React.Component<IRoomProps, IRoomStates> {
             {
               centerStream &&
               (<Video key={`center-${centerStream.id}`} videoStream={centerStream}></Video>)
-
             }
           </div>
           <div className={styles.bottom}>
-            <div>Control</div>
-            <div>Control</div>
-            <div>Control</div>
-            <div>Control</div>
+            <div onClick={() => this.switchVideo()}>Video</div>
+            <div onClick={() => this.switchAudio()}>Audio</div>
           </div>
         </div>
         <div className={styles.right}>
