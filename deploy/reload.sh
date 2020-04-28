c553b2cdaa2dd3b0cf291076034065422df7e625
bryht <bryht@live.com>
2020-04-28 22:56:23 +0200

    add clean image

--- deploy/reload.sh
+++ deploy/reload.sh
@@ -1,2 +1,5 @@
+docker-compose down
 docker-compose pull
 docker-compose up -d
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker rm $(docker ps -qa --no-trunc --filter "status=exited")
