3f4fd0e34bdd1bdd915379c6fa3bf34563007c67
bryht <bryht@live.com>
2020-05-02 12:41:52 +0200

    change link

--- deploy/init-letsencrypt.sh
+++ deploy/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(bryht.net www.bryht.net)
+domains=(letshaveaparty.online www.letshaveaparty.online)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
