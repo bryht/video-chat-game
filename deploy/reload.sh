806965bb3bb793a977bd079de7846512fcc80c5d
bryht <bryht@live.com>
2020-05-05 20:07:38 +0200

    clean

--- deploy/reload.sh
+++ deploy/reload.sh
@@ -1,5 +1,4 @@
 docker-compose down
 docker-compose pull
 docker-compose up -d
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+docker system prune -a -f
