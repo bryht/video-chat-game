311cec8e90b6a16f84c6f334142d1a16f1889a71
bryht <bryht@live.com>
2020-05-26 23:18:51 +0200

    change style

--- src/pages/Welcome/WelcomePage.tsx
+++ src/pages/Welcome/WelcomePage.tsx
@@ -15,17 +15,17 @@ export default class WelcomePage extends React.Component<IWelcomePageProps> {
                     <img src={welcome} alt="" />
                 </div>
                 <div className={styles.welcome}>
-                    <div className={styles.button}>
+                    <div className={styles.card}>
                         <FaDribbbleSquare></FaDribbbleSquare>
                         <a href="/enter/create-party">Create Party</a>
                         <p>create a party with a room name, people can join your party through room name.</p>
                     </div>
-                    <div className={styles.button}>
+                    <div className={styles.card}>
                         <FaMitten></FaMitten>
                         <a href="/enter/join-party">Join Party</a>
                         <p>your friend create a party with a room name, use the name to join.</p>
                     </div>
-                    <div className={styles.button}>
+                    <div className={styles.card}>
                         <FaGamepad></FaGamepad>
                         <a href="/enter/create-game">Create Game</a>
                         <p>create a game on your big screen, people can join through their phone.</p>
